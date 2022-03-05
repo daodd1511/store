384cac5cafe8139b992814d5d6cb56774e354a82
Dao <duongducdao1511@gmail.com>
2022-03-05 23:43:13 +0700

    create homepage

---
+++ client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import env from "../../env.js";
+let data = ref();
+axios
+  .get(`${env.API_URL}/products`)
+  .then((response) => (data.value = response.data));
+</script>
+
+<template>
+  <p>{{ data }}</p>
+</template>
+
+<style></style>

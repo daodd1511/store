c97fc566086a5e9e7ce972483937262e4d1c2077
Dao <duongducdao1511@gmail.com>
2022-03-05 23:43:06 +0700

    create add page

---
+++ client/src/views/Add.vue
@@ -0,0 +1,31 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import env from "../../env.js";
+import router from "../router/router.js";
+let name = ref("");
+let price = ref();
+const post = async () => {
+  const data = {
+    name: name.value,
+    price: price.value,
+  };
+  await axios
+    .post(`${env.API_URL}/add`, data)
+    .then((response) => console.log(response.data))
+    .catch((error) => console.error(error));
+  router.push("/");
+};
+</script>
+<template>
+  <div>Add</div>
+  <form @submit.prevent="post()">
+    <label for="name">Name</label>
+    <input autocomplete="off" type="text" id="name" v-model="name" />
+    <label for="price">Price</label>
+    <input type="text" id="price" v-model="price" />
+    <input type="submit" value="Submit" />
+  </form>
+</template>
+
+<style scoped></style>

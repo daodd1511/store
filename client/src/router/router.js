a0a803cfab114eca5153290fcb249de2aeec63d8
Dao <duongducdao1511@gmail.com>
2022-03-07 02:25:59 +0700

    create update path

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Add from "../views/Add.vue";
 import Home from "../views/Home.vue";
+import Update from "../views/Update.vue";
+
 const routes = [
   {
     path: "/",
@@ -11,6 +13,11 @@ const routes = [
     path: "/add",
     name: "Add",
     component: Add,
+  },
+  {
+    path: "/update/:id",
+    name: "Update",
+    component: Update,
   },
 ];
 const router = createRouter({

a85114eab06e6ab02fd5100ce613b3e0bbf76113
Dao <duongducdao1511@gmail.com>
2022-03-07 02:25:42 +0700

    use pinia

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router.js";
+import { createPinia } from "pinia";
 const app = createApp(App);
-app.use(router).mount("#app");
+app.use(router).use(createPinia()).mount("#app");

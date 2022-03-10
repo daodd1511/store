90261d18c698d0714a46b216e7939644a1b30b96
Dao <duongducdao1511@gmail.com>
2022-03-11 01:19:56 +0700

    add different get product function

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,12 +2,26 @@
 import { onBeforeMount, ref } from "vue";
 import { useStore } from "../store/store.js";
 const store = useStore();
+const category = ref("");
+const productsData = ref();
 onBeforeMount(async () => {
-  await store.getAllProducts();
+  getAllLaptop();
 });
+const getAllPhone = async () => {
+  category.value = "phone";
+  await store.getAllProducts(category.value);
+};
+const getAllLaptop = async () => {
+  category.value = "laptop";
+  await store.getAllProducts(category.value);
+};
 </script>
 
 <template>
+  <nav>
+    <button @click="getAllLaptop">Laptop</button>
+    <button @click="getAllPhone">Phone</button>
+  </nav>
   <ul>
     <li v-for="item in store.allProduct">
       {{ item._id }} - {{ item.general.name }} - {{ item.general.price }}$
@@ -16,10 +30,15 @@ onBeforeMount(async () => {
       <router-link :to="`/update?category=${item.category}&id=${item._id}`"
         ><button>Update</button></router-link
       >
-      <button @click="store.deleteProduct(item._id)">Delete</button>
+      <button @click="store.deleteProduct(item._id, item.category)">
+        Delete
+      </button>
     </li>
   </ul>
-  <router-link to="/add"><button>Add</button></router-link>
+  <router-link to="/add?category=laptop"
+    ><button>Add Laptop</button></router-link
+  >
+  <router-link to="/add?category=phone"><button>Add Phone</button></router-link>
 </template>
 
 <style></style>

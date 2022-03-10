e3690771c91d8319236c6b9781e010ea8fb960ea
Dao <duongducdao1511@gmail.com>
2022-03-10 18:01:43 +0700

    add dev env

--- client/env.js
+++ client/env.js
@@ -1,2 +1,5 @@
-const API_URL = "http://localhost:4000/api";
+const isDev = true;
+const API_URL = isDev
+  ? "http://localhost:4000/api"
+  : "https://ducdao-nodejs-api.herokuapp.com/api";
 export default { API_URL };

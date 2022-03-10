e75cb950c6456956fbe12d55f5c76a7a64f21001
Dao <duongducdao1511@gmail.com>
2022-03-10 18:03:52 +0700

    change handle data logic

--- client/src/views/Update.vue
+++ client/src/views/Update.vue
@@ -4,27 +4,46 @@ import { onBeforeMount, ref } from "vue";
 import { useRoute } from "vue-router";
 const store = useStore();
 const route = useRoute();
+let productData = ref();
 store.status = 0;
 onBeforeMount(async () => {
-  await store.getProductById(route.params.id);
+  await store.getProduct(route.query.id, route.query.category);
+  productData.value = store.product;
 });
 const update = async () => {
   await store.updateProduct(route.params.id);
 };
 </script>
 <template>
-  <form @submit.prevent="update()">
-    <label for="name">Name</label>
-    <input type="text" id="name" v-model="store.product.name" />
+  <div v-if="productData">
+    <form @submit.prevent="update()">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="productData.general.name" />
+      </div>
+      <div>
+        <label for="price">Price</label>
+        <input type="text" id="price" v-model="productData.general.price" />
+      </div>
+      <div>
+        <label for="img_md">Image</label>
+        <input type="text" id="img_md" v-model="productData.image.medium" />
+        <a :href="productData.image.medium" target="blank">Preview</a>
+      </div>
+      <div>
+        <label for="img_lg">Image</label>
+        <input type="text" id="img_lg" v-model="productData.image.large" />
+        <a :href="productData.image.large" target="blank">Preview</a>
+      </div>
+      <input type="submit" value="Update" />
+    </form>
     <br />
-    <label for="price">Price</label>
-    <input type="text" id="price" v-model="store.product.price" /><br />
-    <label for="image">Image</label>
-    <input type="text" id="image" v-model="store.product.image" />
-    <a :href="store.product.image" target="blank">Preview</a><br />
-    <input type="submit" value="Update" /><br />
-  </form>
-  <p v-if="store.status == 200">Success!</p>
+    <p v-if="store.status == 200">Success!</p>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+input {
+  width: 70%;
+}
+</style>

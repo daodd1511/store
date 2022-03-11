dc162f77c533b2520f3bdde37cbb1e3285987110
Dao <duongducdao1511@gmail.com>
2022-03-11 15:23:54 +0700

    deploy

---
+++ src/models/phone.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const phoneSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    general: {
+      name: { type: String, required: true },
+      brand: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    image: { type: String, required: true },
+    resolution: { type: String, required: true },
+    keyspecs: {
+      operating_system: { type: String, required: true },
+      storage: { type: Number, required: true },
+      memory: { type: Number, required: true },
+      processor: { type: String, required: true },
+      front_camera: { type: Number, required: true },
+      rear_camera: { type: Number, required: true },
+    },
+  },
+  { timestamps: true }
+);
+const Phone = mongoose.model("phones", phoneSchema);
+export default Phone;

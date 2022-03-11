b356f114adde49bd663b3c51de81a56b8aa90abf
Dao <duongducdao1511@gmail.com>
2022-03-11 15:22:38 +0700

    change hello

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello from ducdao1511");
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {

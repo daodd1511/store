a12e31ec9b611c4e5ee11ebc1bae35e58bb20e29
Dao <duongducdao1511@gmail.com>
2022-04-08 19:13:48 +0700

    add product page

---
+++ client/src/components/Product.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  data: Object,
+});
+const product = props.data;
+</script>
+<template>
+  <div class="min-h-60 mx-auto flex w-4/5 gap-8 py-7">
+    <!-- Image section -->
+    <div class="w-1/3">
+      <router-link :to="`/laptop/` + product._id">
+        <img
+          :src="product.image"
+          alt="Product Image"
+          class="mx-auto max-h-52 max-w-full"
+        />
+      </router-link>
+    </div>
+    <!-- Content section -->
+    <div class="h-full w-2/5 text-blue-600">
+      <router-link :to="`/laptop/` + product._id">{{
+        product.general.name
+      }}</router-link>
+    </div>
+    <!-- Price section -->
+    <div class="h-full w-1/5">
+      <p class="text-2xl font-medium">${{ product.general.price }}</p>
+      <div v-if="product.general.sale_price" class="text-[13px]">
+        <!-- Discount box -->
+        <div class="mr-1 inline-block bg-red-700 p-1 font-medium text-white">
+          Save ${{ product.general.price - product.general.sale_price }}
+        </div>
+        <span>Was ${{ product.general.sale_price }}</span>
+      </div>
+      <!-- Add to cart button -->
+      <button class="my-6 h-8 w-full rounded bg-yellow-300 text-sm font-medium">
+        <i class="fa-solid fa-cart-shopping p-2"></i>
+        Add to cart
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>

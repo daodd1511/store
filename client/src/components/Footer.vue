000af02cf32976455b0020e78742a4842739aef6
Dao <duongducdao1511@gmail.com>
2022-04-10 16:10:45 +0700

    create footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <div class="flex h-16 w-full items-center justify-center">
+    Made with <i class="fa-solid fa-heart px-1 text-red-600"></i> by ducdao1511
+  </div>
+</template>
+
+<style lang="scss" scoped></style>

02593bd8fc7b305321d8b1e53e4f86eaccad5da2
Dao <duongducdao1511@gmail.com>
2022-04-10 16:12:57 +0700

    create phone page

---
+++ client/src/views/products/Phone.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from "../../components/Header.vue";
+import { onBeforeMount, ref } from "vue";
+import { useStore } from "../../store/store.js";
+import Product from "../../components/Product.vue";
+const store = useStore();
+const category = ref("");
+onBeforeMount(async () => {
+  getAllPhone();
+});
+const getAllPhone = async () => {
+  category.value = "phone";
+  await store.getAllProducts(category.value);
+};
+</script>
+<template>
+  <Header></Header>
+  <Product
+    v-for="item in store.allProduct"
+    :key="item._id"
+    :data="item"
+  ></Product>
+</template>
+
+<style lang="scss" scoped></style>

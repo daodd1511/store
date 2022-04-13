65191a7010099612866dc4678f6a27ef6e6d909a
Dao <duongducdao1511@gmail.com>
2022-04-13 23:09:51 +0700

    add home page

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+// Import product views
 import Laptop from "../views/products/Laptop.vue";
 import Phone from "../views/products/Phone.vue";
+import ProductDetail from "../views/products/ProductDetail.vue";
+// Import admin views
 import AdminHome from "../views/admin/AdminHome.vue";
 import AdminProduct from "../views/admin/AdminProduct.vue";
 import Add from "../views/admin/Add.vue";
 import Update from "../views/admin/Update.vue";
-import ProductDetail from "../views/products/ProductDetail.vue";
 const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
   {
     path: "/laptop",
     name: "Laptop",

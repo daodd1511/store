ef34174b7ac60013adc649dd3ef4032efdbd031d
Dao <duongducdao1511@gmail.com>
2022-04-13 22:57:21 +0700

    set width for container

--- client/src/views/admin/AdminProduct.vue
+++ client/src/views/admin/AdminProduct.vue
@@ -6,15 +6,15 @@ const route = useRoute();
 const store = useStore();
 </script>
 <template>
-  <div>
+  <div class="mx-auto w-11/12">
     <router-link :to="'/admin/add?category=' + route.params.category">
-      <button class="h-8 w-20 rounded-md bg-green-500">
+      <button class="float-right h-8 w-20 rounded-md bg-green-400">
         <i class="fa-solid fa-plus"></i>
         Add
       </button>
     </router-link>
+    <ProductTable :data="store.allProduct"></ProductTable>
   </div>
-  <ProductTable :data="store.allProduct"></ProductTable>
 </template>
 
 <style scoped></style>

e4a4cca8fe237b5c1b8947b22e5f9713a58a58cd
Dao <duongducdao1511@gmail.com>
2022-04-13 16:16:06 +0700

    add nested view

--- client/src/views/admin/AdminHome.vue
+++ client/src/views/admin/AdminHome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onBeforeMount, ref } from "vue";
 import { useStore } from "../../store/store.js";
+import Sidebar from "../../components/admin/Sidebar.vue";
 const store = useStore();
 const category = ref("");
 onBeforeMount(async () => {
@@ -15,80 +16,16 @@ const getAllLaptop = async () => {
   category.value = "laptop";
   await store.getAllProducts(category.value);
 };
+const emit = defineEmits(["laptop", "phone"]);
 </script>
 
 <template>
-  <nav class="navbar">
-    <img src="../../assets/logo.png" alt="" />
-    <div class="navbar-link">
-      <div @click="getAllLaptop" class="navbar-item">LAPTOP</div>
-      <div @click="getAllPhone" class="navbar-item">PHONE</div>
+  <main class="pb-16">
+    <Sidebar @laptop="getAllLaptop" @phone="getAllPhone"></Sidebar>
+    <div class="pl-60 pt-10">
+      <router-view></router-view>
     </div>
-  </nav>
-  <router-link to="/admin/add?category=laptop" v-if="category == 'laptop'"
-    ><button class="btn">Add Laptop</button></router-link
-  >
-  <router-link to="/admin/add?category=phone" v-if="category == 'phone'"
-    ><button class="btn">Add Phone</button></router-link
-  >
-  <ul class="product-container">
-    <li v-for="item in store.allProduct">
-      <div class="product-img">
-        <img :src="item.image" alt="Product image" />
-      </div>
-      <p class="product-name">{{ item.general.name }}</p>
-      <div class="product-button">
-        <button class="btn">
-          <router-link
-            :to="`/admin/update?category=${item.category}&id=${item._id}`"
-            >Update</router-link
-          >
-        </button>
-        <button
-          @click="store.deleteProduct(item._id, item.category)"
-          class="btn"
-        >
-          Delete
-        </button>
-      </div>
-    </li>
-  </ul>
+  </main>
 </template>
 
-<style scoped>
-img {
-  max-width: 100%;
-}
-
-.navbar {
-  display: flex;
-  background-color: #131313;
-  height: 80px;
-  color: white;
-  align-items: center;
-  padding: 0 20px;
-}
-.navbar img {
-  height: 70%;
-}
-.navbar-link {
-  display: flex;
-  padding-left: 10%;
-  gap: 40px;
-}
-.navbar-item {
-  cursor: pointer;
-}
-.product-container {
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-  padding: 20px;
-  gap: 20px;
-}
-.product-container li {
-  list-style-type: none;
-}
-.product-name {
-  padding-top: 20px;
-}
-</style>
+<style scoped></style>

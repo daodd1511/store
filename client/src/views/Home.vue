4eacaa7a44a5b4a1a8650f306dd08475176da3e8
Dao <duongducdao1511@gmail.com>
2022-04-13 23:09:58 +0700

    create home page

---
+++ client/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <div>
+    <router-link to="/laptop"
+      ><button class="h-screen w-1/2 bg-teal-300">Laptop</button></router-link
+    >
+    <router-link to="/phone"
+      ><button class="h-screen w-1/2 bg-rose-300">Phone</button></router-link
+    >
+  </div>
+</template>
+
+<style scoped></style>

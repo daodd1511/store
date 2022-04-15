f53bdd0aa1db272571376319e3699cd19d131611
Dao <duongducdao1511@gmail.com>
2022-04-15 17:02:41 +0700

    add new product table design

--- client/src/views/admin/AdminProduct.vue
+++ client/src/views/admin/AdminProduct.vue
@@ -6,13 +6,13 @@ const route = useRoute();
 const store = useStore();
 </script>
 <template>
-  <div class="mx-auto w-11/12">
-    <router-link :to="'/admin/add?category=' + route.params.category">
+  <div class="mx-auto w-10/12">
+    <!-- <router-link :to="'/admin/add?category=' + route.params.category">
       <button class="float-right h-8 w-20 rounded-md bg-green-400">
         <i class="fa-solid fa-plus"></i>
         Add
       </button>
-    </router-link>
+    </router-link> -->
     <ProductTable :data="store.allProduct"></ProductTable>
   </div>
 </template>

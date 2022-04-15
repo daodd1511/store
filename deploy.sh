47814c359d11b7e1b553d35d088a1b1ef29b1a42
Dao <duongducdao1511@gmail.com>
2022-04-15 20:48:02 +0700

    remove deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/daodd1511/store.git master:server

f23257e405b7a8ed2fec22678faa0bbb35e10728
Dao <duongducdao1511@gmail.com>
2022-04-16 23:51:36 +0700

    add style

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,18 @@
 <script setup></script>
 <template>
-  <div>
-    <router-link to="/laptop"
-      ><button class="h-screen w-1/2 bg-teal-300 text-4xl">
-        Laptop
+  <div class="w-full flex-col sm:flex sm:flex-row">
+    <router-link to="/laptops"
+      ><button
+        class="h-[50vh] w-screen bg-teal-300 text-4xl sm:h-screen sm:w-[50vw]"
+      >
+        Laptops
       </button></router-link
     >
-    <router-link to="/phone"
-      ><button class="h-screen w-1/2 bg-rose-300 text-4xl">
-        Phone
+    <router-link to="/phones"
+      ><button
+        class="h-[50vh] w-screen bg-rose-300 text-4xl sm:h-screen sm:w-[50vw]"
+      >
+        Phones
       </button></router-link
     >
   </div>

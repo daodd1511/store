137db416a05f9cb78f6ad7afcb506d4b823e9af0
Dao <duongducdao1511@gmail.com>
2022-04-16 15:36:08 +0700

    change font size

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,10 +2,14 @@
 <template>
   <div>
     <router-link to="/laptop"
-      ><button class="h-screen w-1/2 bg-teal-300">Laptop</button></router-link
+      ><button class="h-screen w-1/2 bg-teal-300 text-4xl">
+        Laptop
+      </button></router-link
     >
     <router-link to="/phone"
-      ><button class="h-screen w-1/2 bg-rose-300">Phone</button></router-link
+      ><button class="h-screen w-1/2 bg-rose-300 text-4xl">
+        Phone
+      </button></router-link
     >
   </div>
 </template>

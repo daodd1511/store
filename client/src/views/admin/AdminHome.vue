d5375a68b6da33e94db77e6d25bfa5149c316e03
Dao <duongducdao1511@gmail.com>
2022-04-17 15:37:51 +0700

    remove getting data and change emit action

--- client/src/views/admin/AdminHome.vue
+++ client/src/views/admin/AdminHome.vue
@@ -1,32 +1,17 @@
 <script setup>
-import { onBeforeMount, ref } from "vue";
-import { useStore } from "../../store/store.js";
-import { useRoute } from "vue-router";
+import { useRouter } from "vue-router";
 import Sidebar from "../../components/admin/Sidebar.vue";
-const store = useStore();
-const route = useRoute();
-const category = ref("");
-onBeforeMount(async () => {
-  if (route.params.category == "laptop") {
-    getAllLaptop();
-  } else if (route.params.category == "phone") {
-    getAllPhone();
-  }
-});
-const getAllPhone = async () => {
-  category.value = "phone";
-  await store.getAllProducts(category.value);
-};
-const getAllLaptop = async () => {
-  category.value = "laptop";
-  await store.getAllProducts(category.value);
-};
+const router = useRouter();
+
 const emit = defineEmits(["laptop", "phone"]);
 </script>
 
 <template>
   <main class="pb-16">
-    <Sidebar @laptop="getAllLaptop" @phone="getAllPhone"></Sidebar>
+    <Sidebar
+      @laptop="router.replace('/admin/products/laptop')"
+      @phone="router.replace('/admin/products/phone')"
+    ></Sidebar>
     <div class="pl-60 pt-10">
       <router-view></router-view>
     </div>

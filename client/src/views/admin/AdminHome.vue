a005b2123081ab0c5a9bd7295371aad5eba23ab5
Dao <duongducdao1511@gmail.com>
2022-04-20 19:14:10 +0700

    add welcome message on admin home page

--- client/src/views/admin/AdminHome.vue
+++ client/src/views/admin/AdminHome.vue
@@ -1,14 +1,20 @@
 <script setup>
-import { useRouter } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 import Sidebar from "../../components/admin/Sidebar.vue";
 const router = useRouter();
-
-const emit = defineEmits(["laptop", "phone"]);
+const route = useRoute();
+const emit = defineEmits(["laptop", "phone", "home"]);
 </script>
 
 <template>
   <main class="pb-16">
+    <div v-if="route.path == '/admin'" class="h-[77vh] pl-60 pt-10 text-center">
+      <h1 class="p-8 text-3xl">Welcome to admin page</h1>
+      <h2 class="text-xl">Please select product category on the sidebar</h2>
+    </div>
+
     <Sidebar
+      @home="router.replace('/admin')"
       @laptop="router.replace('/admin/products/laptop')"
       @phone="router.replace('/admin/products/phone')"
     ></Sidebar>

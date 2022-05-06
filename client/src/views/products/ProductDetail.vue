a98a88022e422f2d44bcf6fb8b1a0c7d6e65febd
Dao <duongducdao1511@gmail.com>
2022-05-06 17:48:49 +0700

    change variable nam

--- client/src/views/products/ProductDetail.vue
+++ client/src/views/products/ProductDetail.vue
@@ -6,53 +6,52 @@ import Navbar from "../../components/Navbar.vue";
 import Spinner from "../../components/Spinner.vue";
 const store = useStore();
 const route = useRoute();
-const productData = ref();
+const product = ref();
 onBeforeMount(async () => {
   await store.getProductById(route.params.id, route.params.category);
-  productData.value = store.product;
+  product.value = store.product;
 });
 </script>
 <template>
   <Navbar></Navbar>
-  <div v-if="!productData">
+  <div v-if="!product">
     <Spinner></Spinner>
   </div>
   <div v-else class="mx-auto w-11/12 pt-20 sm:w-4/5">
     <div class="flex w-full flex-col py-7 sm:flex-row sm:gap-14">
       <!-- Name and image section -->
       <div class="w-full font-medium sm:w-2/3">
-        <h2>{{ productData.general.brand }}</h2>
-        <h1 class="text-2xl font-light">{{ productData.general.name }}</h1>
-        <img :src="productData.image" class="py-12" />
+        <h2>{{ product.general.brand }}</h2>
+        <h1 class="text-2xl font-light">{{ product.general.name }}</h1>
+        <img :src="product.image" class="py-12" />
       </div>
       <!-- Price and add to cart section -->
       <div class="h-full w-full sm:w-1/4">
         <p
           class="text-3xl font-medium sm:text-2xl"
-          v-if="!productData.general.sale_price"
+          v-if="!product.general.sale_price"
         >
-          ${{ productData.general.price }}
+          ${{ product.general.price }}
         </p>
         <div v-else class="w-full items-center text-sm sm:text-[13px]">
           <p
             class="mr-6 inline-block text-3xl font-medium sm:mr-2 sm:block sm:text-2xl"
           >
-            ${{ productData.general.sale_price }}
+            ${{ product.general.sale_price }}
           </p>
           <!-- Discount box -->
           <div>
             <div
               class="mr-1 inline-block w-fit rounded bg-red-700 p-1 font-medium text-white"
             >
-              Save ${{
-                productData.general.price - productData.general.sale_price
-              }}
+              Save ${{ product.general.price - product.general.sale_price }}
             </div>
-            <span>Was ${{ productData.general.price }}</span>
+            <span>Was ${{ product.general.price }}</span>
           </div>
         </div>
         <!-- Add to cart button -->
         <button
+          @click="store.addToCart(product)"
           class="my-6 h-12 w-full items-center rounded bg-yellow-300 text-center text-lg font-medium sm:h-8 sm:text-sm"
         >
           <i class="fa-solid fa-cart-shopping p-2"></i>
@@ -70,59 +69,47 @@ onBeforeMount(async () => {
         <ul>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Product Name</div>
-            <div class="w-1/2">{{ productData.general.name }}</div>
+            <div class="w-1/2">{{ product.general.name }}</div>
           </li>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Brand</div>
-            <div class="w-1/2">{{ productData.general.brand }}</div>
+            <div class="w-1/2">{{ product.general.brand }}</div>
           </li>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Price</div>
-            <div class="w-1/2">${{ productData.general.price }}</div>
+            <div class="w-1/2">${{ product.general.price }}</div>
           </li>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Processor</div>
-            <div class="w-1/2">{{ productData.processor }}</div>
+            <div class="w-1/2">{{ product.processor }}</div>
           </li>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Storage</div>
-            <div class="w-1/2">{{ productData.storage }} GB</div>
+            <div class="w-1/2">{{ product.storage }} GB</div>
           </li>
           <li class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Memory</div>
-            <div class="w-1/2">{{ productData.memory }} GB</div>
+            <div class="w-1/2">{{ product.memory }} GB</div>
           </li>
           <!-- Laptop screen -->
-          <li
-            v-if="productData.category == 'laptop'"
-            class="flex py-2 text-[16px]"
-          >
+          <li v-if="product.category == 'laptop'" class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Screen</div>
-            <div class="w-1/2">{{ productData.screen }}"</div>
+            <div class="w-1/2">{{ product.screen }}"</div>
           </li>
           <!-- Laptop graphics -->
-          <li
-            v-if="productData.category == 'laptop'"
-            class="flex py-2 text-[16px]"
-          >
+          <li v-if="product.category == 'laptop'" class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Graphics</div>
-            <div class="w-1/2">{{ productData.graphics }}</div>
+            <div class="w-1/2">{{ product.graphics }}</div>
           </li>
           <!-- Phone camera -->
-          <li
-            v-if="productData.category == 'phone'"
-            class="flex py-2 text-[16px]"
-          >
+          <li v-if="product.category == 'phone'" class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Camera</div>
-            <div class="w-1/2">{{ productData.camera }} MP</div>
+            <div class="w-1/2">{{ product.camera }} MP</div>
           </li>
           <!-- Phone resolution -->
-          <li
-            v-if="productData.category == 'phone'"
-            class="flex py-2 text-[16px]"
-          >
+          <li v-if="product.category == 'phone'" class="flex py-2 text-[16px]">
             <div class="w-1/2 font-medium">Resolution</div>
-            <div class="w-1/2">{{ productData.resolution }}</div>
+            <div class="w-1/2">{{ product.resolution }}</div>
           </li>
         </ul>
       </div>

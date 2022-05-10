fbcde1756315356e4c5c7ef035a930a529efe4a2
Dao <duongducdao1511@gmail.com>
2022-05-10 12:27:42 +0700

    change emit

--- client/src/components/Toast.vue
+++ client/src/components/Toast.vue
@@ -1,10 +1,4 @@
-<script setup>
-import { onBeforeMount } from "vue";
-const emit = defineEmits(["closeToastByButton", "autoCloseToast"]);
-onBeforeMount(() => {
-  emit("autoCloseToast");
-});
-</script>
+<script setup></script>
 <template>
   <div
     class="mb-4 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow-lg dark:bg-gray-800 dark:text-gray-400"
@@ -30,7 +24,7 @@ onBeforeMount(() => {
       <slot></slot>
     </div>
     <button
-      @click="emit('closeToastByButton')"
+      @click="$emit('closeToastByButton')"
       type="button"
       class="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
       data-dismiss-target="#toast-danger"
